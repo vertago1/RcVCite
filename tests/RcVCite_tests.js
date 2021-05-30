935b796089a01d25bc20ea00d5dfd009eecc2596
Allen Webb <vertago1@gmail.com>
2021-05-30 12:07:11 -0500

    First pass at cleaning up tests.
    This also imports RcVCite_iframe_tests.html, and adds barebones npm
    package.json to faciliate easy testing using npm test.

--- tests/RcVCite_tests.js
+++ tests/RcVCite_tests.js
@@ -1,49 +1,63 @@
-var fs = require('fs');
 
-function source_file(filename) {
-  console.log('Loading '+filename);
-  eval(fs.readFileSync(filename)+'');
-  console.log('SUCCESS!');
-}
+module.exports.tests = () => {
+  var fs = require('fs');
+  var path = require("path");
 
-//source_file('jquery.js');
-//source_file('versification.js');
-//source_file('RcVCite_v00_04.js');
+  var jsdom = require('jsdom');
+  const JSDOM = jsdom.JSDOM;
 
-var jsdom = require('jsdom');
+  run_test = (file, dom_factory) => {
+    var error_list = [];
 
-const JSDOM = jsdom.JSDOM;
+    var virtualConsole = new jsdom.VirtualConsole();
+    virtualConsole.sendTo(console);
+    virtualConsole.addListener("jsdomError", (error) => {
+      error_list.push(error);
+    });
 
-run_test = dom => {
-  //dom.window.eval('jQuery("body").append("<span>Hello World!</span>");');
-  var countdown = 100;
-  var check_ready = function(){
-    if (!dom.window.tests_finished) {
-      countdown--;
-      if (countdown > 0) {
-        setTimeout(check_ready, 5);
+    var dom = dom_factory({
+      'resources': 'usable',
+      'runScripts': 'dangerously',
+      'url': 'file://' + file,
+      virtualConsole});
+    
+    var countdown = 100;
+    var check_ready = () => {
+      if (!dom.window.tests_finished) {
+        countdown--;
+        if (countdown > 0) {
+          setTimeout(check_ready, 5);
+        } else {
+          var msg = 'TIMEOUT REACHED!';
+          console.log(msg);
+          throw msg;
+        }
       } else {
-        console.log('TIMEOUT REACHED!');
-        process.exit(1);
+        if (dom.window.error_list && dom.window.error_list.length > 0) {
+          console.error(dom.window.error_list);
+          for (var x = 0; x < dom.window.error_list.length; ++x) {
+            error_list.push(dom.window.error_list[x]);
+          }
+        }
+        if (error_list.length > 0) {
+          //
+          throw error_list;
+        }
       }
-    } else {
-      if (dom.window.error_list.length > 0){
-        process.exit(1);
-      }
-      //console.log(dom.serialize());
-    }
+    };
+    check_ready();
   };
-  check_ready();
-  
-  //var $ = window.jQuery;
 
-  //$('body').append("<div class='testing'>Hello World</div>");
-  //console.log($(".testing").text()); // outputs Hello World
+  var file = path.resolve('tests/RcVCite_tests.html');
+  console.debug("Loading:", file);
+  fs.readFile(file, (err, buffer) => {
+    if (err) {
+      console.error(err);
+      throw err;
+    }
+    run_test(file, (kwargs) => {
+      return new JSDOM(buffer, kwargs);
+    });
+    //run_test(new JSDOM((buffer + '').replace('src="./jquery.js"', 'src="./jquery.1.10.2.min.js"'), {resources: 'usable', runScripts: 'dangerously'}));
+  });
 };
-
-fs.readFile("RcVCite_tests.html", (err, buffer) => {
-  run_test(new JSDOM(buffer, {resources: 'usable', runScripts: 'dangerously'}));
-  run_test(new JSDOM((buffer+'').replace('src="./jquery.js"','src="./jquery.1.10.2.min.js"'), {resources: 'usable', runScripts: 'dangerously'}));
-});
-
-//JSDOM.fromFile("RcVCite_tests.html", {resources: 'usable', runScripts: 'dangerously'}).then(run_test);
---
+++ tests/test.js
@@ -0,0 +1,9 @@
+(function(){
+  var versifiction_tests = require('./versification_tests');
+  versifiction_tests.tests();
+})();
+
+(function(){
+  var RcVcite_tests = require('./RcVCite_tests');
+  RcVcite_tests.tests();
+})();
